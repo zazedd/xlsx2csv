9269a37f5095ddfd9096c567a2b09b8c34d0409b
zazedd <leomendesantos@gmail.com>
2024-10-10 18:22:49 +0100

    fix `encoding_to_xmlm_encoding` typo

--- src/xmlt.ml
+++ src/xmlt.ml
@@ -10,5 +10,5 @@ type encoding = [ `UTF_8 | `UTF_16 | `UTF_16BE | `UTF_16LE ]
 let encoding_to_xmlm_encoding : encoding -> Xmlm.encoding = function
   | `UTF_8 -> `UTF_8
   | `UTF_16 -> `UTF_16
-  | `UTF_16BE -> `UTF_16LE
-  | `UTF_16LE -> `UTF_16BE
+  | `UTF_16LE -> `UTF_16LE
+  | `UTF_16BE -> `UTF_16BE
